e43ee4d2865e59fe8a3fd135848f571271f2b62a
jediemil <emil.reyier.hedin@gmail.com>
2022-05-10 10:53:17 +0200

    Another lektion has done the funkterererera. Now i wil be burgurgering to be of the donene.
    Mobilanpassade alla sidor och lade till en ny meny för mobila enheter.

--- js/topBar.js
+++ js/topBar.js
@@ -23,11 +23,32 @@ function hide() {
     topBar.style.animation = ""
 }*/
 document.getElementById("topBar").style.transform = "translateY(-100%)"
+document.getElementById("topBarMobile").style.transform = "translateY(-100%)"
 
 window.onscroll = function() {
     if (window.innerHeight/2 < window.scrollY) {
         document.getElementById("topBar").style.transform = "translateY(0%)"
+        document.getElementById("topBarMobile").style.transform = "translateY(0%)"
     } else {
         document.getElementById("topBar").style.transform = "translateY(-100%)"
+        document.getElementById("topBarMobile").style.transform = "translateY(-100%)"
     }
+}
+
+function dropDownProject() {
+    document.getElementById("myProjectsDropdown").classList.remove("hidden")
+}
+function toggleDownProject() {
+    document.getElementById("myProjectsDropdown").classList.toggle("hidden")
+}
+function hideProject() {
+    document.getElementById("myProjectsDropdown").classList.add("hidden")
+}
+
+function toggleMenuMobile() {
+    document.getElementById("menuMobile").classList.toggle("hidden")
+}
+function toggleDownProjectMobile() {
+    document.getElementById("dropdownMobile").classList.toggle("hidden")
+    document.getElementById("menuMobile").classList.toggle("hidden")
 }
