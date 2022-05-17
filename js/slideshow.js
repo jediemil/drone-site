38d45ae704a27d1142058fcc3ce715e606289a85
jediemil <emil.reyier.hedin@gmail.com>
2022-05-17 09:48:36 +0200

    Kommentarer

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,11 +2,12 @@ let currentSlide = 0
 
 const slides = document.getElementById("projectImageWithCap").getElementsByClassName("imageSegment")
 
-function changeSlide(direction) {
+function changeSlide(direction) { //Flyttar mellan bilderna.
+    // Direction är ett tal som säger hur många bilder framåt eller bakåt det ska flytta.
     slides[currentSlide].classList.add("invisible")
     currentSlide += direction
     currentSlide %= slides.length
-    if (currentSlide < 0) {
+    if (currentSlide < 0) { //Modulus fungerar inte i JS (för varför skulle det?), så behöver en extra säkerhetscheck. -1 mod 5 ska vara 4, inte -1.
         currentSlide = slides.length - 1
     }
     slides[currentSlide].classList.remove("invisible")
